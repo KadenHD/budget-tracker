b47d7f4e8e5bb61f820b760c4a060c1dc5e9fc94
KadenHD <71968563+KadenHD@users.noreply.github.com>
2026-03-15 16:12:23 +0100

    cosmetics

--- backend/app/routers/defaults.py
+++ backend/app/routers/defaults.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.controllers import defaults_controller
 
 defaults_router = APIRouter(tags=["defaults"])
+
 defaults_router.get("/")(defaults_controller.get_root)
 defaults_router.get("/health")(defaults_controller.get_health)
 defaults_router.get("/ping")(defaults_controller.get_ping)
