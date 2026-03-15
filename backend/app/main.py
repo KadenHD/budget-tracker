d94db2483ad37a2415416e124715d68f4e72e7b8
KadenHD <71968563+KadenHD@users.noreply.github.com>
2026-03-15 16:19:01 +0100

    add favicon

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import uvicorn
 from app.config import Config
 from app.routers import (
@@ -17,6 +18,10 @@ app = FastAPI(
     title=f"Budget Tracker ({config.ENV})",
     description="A simple and intuitive budget tracker that helps users manage their finances efficiently. Users can create accounts, log transactions, assign categories, and visualize their spending with detailed statistics.",
 )
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.ico")
 
 app.include_router(defaults_router)
 app.include_router(auth_router)
